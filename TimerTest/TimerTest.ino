a0004bdff74602b187f21f6f938b71ccc7d00788
Morgan Tenney <morgan.tenney@gmail.com>
2014-03-01 18:35:34 -0800

    Merp

---
+++ TimerTest/TimerTest.ino
@@ -0,0 +1,27 @@
+// TimerTest.ino
+#include "Timer.h"
+
+int counter;
+Timer timer1(5000);
+
+bool done = false;
+
+void setup() {
+	timer1.start();
+	Serial.begin(9600);
+	counter = 0;
+	Serial.print("CURRENT TIME: ");
+	Serial.println(millis());
+}
+
+void loop() {
+	if (timer1.isExpired() && !done){
+		counter++;
+		Serial.println("Timer1 expired");
+		//Serial.println(counter);
+		done = true;
+		Serial.print("FINAL TIME: ");
+		Serial.println(millis());
+	}
+}
+
